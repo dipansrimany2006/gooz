462bbd72d8183fa3d4e1d6d1887c258a29847a0e
dipansrimany2006 <dipansrimany@gmail.com>
2025-09-25 17:34:01 +0530

    /play created and the assets of home page added

---
+++ src/app/play/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { Button } from '@/components/ui/button';
+import Card from "../../components/card";
+import { useState } from 'react';
+import Sidepanel from '@/components/sidepanel';
+import Board from '@/components/board';
+import ConnectWalletButton from '../../../components/ConnectWalletButton';
+import ChatRoom from '@/components/chatRoom';
+import { GameProvider } from '@/context/GameContext';
+
+export default function Home() {
+
+  return (
+    <GameProvider>
+      <div className="h-screen w-screen p-0 m-0 bg-[url('/Gooz_bg.png')] bg-cover bg-center bg-no-repeat">
+        <div className='flex items-end justify-between px-20 h-20'>
+          <img src={"/GoozDotFun.png"} alt=''/>
+          <ConnectWalletButton/>
+        </div>
+        <div className="flex items-center justify-evenly h-[calc(100vh-120px)]">
+          <Sidepanel roomid="12kfn345" />
+          <div className='grid place-items-center'>
+            <Board />
+          </div>
+          <ChatRoom />
+        </div>
+      </div>
+    </GameProvider>
+  );
+}
